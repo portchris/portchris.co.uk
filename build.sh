d56a2a234d59cf22dbe69ccf12a49bb5e2757981
portchris <chris@portchris.co.uk>
2020-05-19 13:23:19 +1000

    Allow non-interaction mode in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CURR_DIR=$(pwd)
+NO_CACHE="--no-cache"
 
 if [ -z ${USER_ID+x} ]; then
 	USER_ID=$(id -u)
@@ -8,6 +9,10 @@ fi
 
 if [ -z ${GROUP_ID+x} ]; then
 	GROUP_ID=$(id -g)
+fi
+
+if [[ $* == *-n* ]]; then
+    NO_CACHE="--pull"
 fi
 
 if [ -f ./.env ]; then
@@ -23,13 +28,18 @@ if [ -f ./.env ]; then
 	cp ./.env $DOCKER_COMPOSE_PATH
 	cp ./$DOCKER_COMPOSE_FILE $DOCKER_COMPOSE_PATH
 	cd $DOCKER_COMPOSE_PATH
-	read -p "Use Docker cache? [Y/y]? " -n 1 -r
-	echo # (optional) move to a new line
-	if [[ $REPLY =~ ^[Yy]$ ]]; then 
-		docker-compose -f $DOCKER_COMPOSE_FILE build --build-arg UID=$USER_ID --build-arg GID=$GROUP_ID workspace $DOCKER_COMPOSE_SERVICES $@
-	else
-		docker-compose -f $DOCKER_COMPOSE_FILE build --pull --no-cache --build-arg UID=$USER_ID --build-arg GID=$GROUP_ID workspace $DOCKER_COMPOSE_SERVICES $@
-	fi
+
+    if [[ $NO_CACHE == "--no-cache" ]]; then
+        read -p "Use Docker cache? [Y/y]? " -n 1 -r
+        echo # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]; then 
+            NO_CACHE="--pull"
+        else 
+            NO_CACHE="--no-cache"
+        fi
+    fi
+	
+    docker-compose -f $DOCKER_COMPOSE_FILE build $NO_CACHE --build-arg UID=$USER_ID --build-arg GID=$GROUP_ID workspace $DOCKER_COMPOSE_SERVICES $2
 else
 	echo "Please create an .env file"
 fi
